cbc868bb2478370083ff890a21ca8249e1fc831b
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-04-15 12:43:00 +0200

    676 step 3: restore cookie checks

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,5 +1,5 @@
 // check if an active campaign is running or OneTrust needs scan the active scripts
-export const COOKIE_CHECK = true;
+export const COOKIE_CHECK = false;
 
 // ONE TRUST COOKIE CONSENT
 export const DATA_DOMAIN_SCRIPT = 'bc931e83-d7f8-4a43-b0d1-402a3ac1f466';
